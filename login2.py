a294aac406ca95cfec95776cb2bb2fd895c60781
Eluviian <bayleysapara@gmail.com>
2023-01-25 17:44:41 -0700

    files for lab 3

---
+++ login2.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import cgi
+import cgitb
+
+cgitb.enable()
+
+import os
+import json
+import secret 
+
+from http.cookies import SimpleCookie
+
+from templates import login_page, secret_page, after_login_incorrect
+
+s = cgi.FieldStorage()
+username = s.getfirst("username")
+password = s.getfirst("password")
+
+form_ok = username == secret.username and password == secret.password
+
+cookie = SimpleCookie(os.environ["HTTP COOKIE"])
+cookie_username = None
+cookie_password = None
+
+if cookie.get("username"):
+    cookie_username = cookie.get("username").value
+if cookie.get("password"):
+    cookie_username = cookie.get("password").value
+
+cookie_ok = cookie_username == secret.username and cookie_password == secret.password
+if cookie_ok:
+    username = cookie_username
+    password = cookie_password
+
+
+if form_ok:
+    print(f"Set-Cookie: username={username}")
+    print(f"Set-Cookie: password={password}")
+
+print("Content-Type: text/html")
+print()
+
+if not username and not password:
+    print(login_page())
+
+elif username== secret.username and password== secret.password:
+    print(secret_page(username, password))
+else:
+    print(login_page())
+    print("username and password: ", username, password)
